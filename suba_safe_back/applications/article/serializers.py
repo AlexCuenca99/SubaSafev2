f4fa417b4ee748fa8c2b84e3a4ba12b526e00f15
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-28 18:06:53 -0500

    Serialize all images in one object

--- suba_safe_back/applications/article/serializers.py
+++ suba_safe_back/applications/article/serializers.py
@@ -1,25 +1,50 @@
+# Imports de DRF
+from dataclasses import fields
 from rest_framework import serializers
 
+# Imports de Modelos
 from .models import Article
-from applications.category.models import Category
+
+# Imports de Serializadores
 from applications.category.serializers import CategorySerializer
+from applications.users.serializers import UserPartialDataSerializer
 
-# class CategorySerializer(serializers.ModelSerializer):
-    # class Meta:
-        # model = Category
-        # fields = (
-            # 'name',
-        # )
-
+# Serializador para los artículos
 class ArticleSerializer(serializers.ModelSerializer):
 
+    images = serializers.SerializerMethodField()
+
     category = CategorySerializer()
-    
+    seller = UserPartialDataSerializer()
+    buyer = UserPartialDataSerializer()
+
     class Meta:
         model = Article
         fields = (
+            'id',
             'name',
             'description',
+            'images',
+            'is_active',
+            'starting_bid',
+            'current_bid',
+            'category',
+            'seller',
+            'buyer',
+            'watchers',
+        )
+    
+    def get_images(self, obj):
+        query_set = Article.article_objects.get(id = obj.id)
+        serialized_objects = ImagesInArticleSerializer(query_set).data
+
+        return serialized_objects
+
+
+class ImagesInArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
             'main_image',
             'main_image_opt_text',
             'image_1',
@@ -30,17 +55,4 @@ class ArticleSerializer(serializers.ModelSerializer):
             'image_3_opt_text',
             'image_4',
             'image_4_opt_text',
-            'is_active',
-            'starting_bid',
-            'current_bid',
-            'category',
-            'seller',
-            'buyer',
-            'watchers',
         )
-
-
-class ArticleSerializerViewSet(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('__all__')
