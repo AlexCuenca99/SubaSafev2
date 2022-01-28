9285f54de2224555d48f8761508b820c0f096e97
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-28 18:09:49 -0500

    Remove permissions for list articles.

--- suba_safe_back/applications/article/viewsets.py
+++ suba_safe_back/applications/article/viewsets.py
@@ -1,18 +1,38 @@
 # Third-Party Apps Imports
-from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny
+)
+from rest_framework import viewsets
 
 
 from .models import Article
 
-from .serializers import ArticleSerializerViewSet
+from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
-    authentication_classes = (TokenAuthentication, JWTAuthentication)
-    permission_classes = [IsAuthenticated]
-
-    serializer_class = ArticleSerializerViewSet
+    # Únicamente los usuarios con un token de acceso podrán 
+    # usar a las operaciones CRUD
+    authentication_classes = (TokenAuthentication,)
     queryset = Article.article_objects.all()
+    serializer_class = ArticleSerializer
+
+    # Permisos para las aplicaciones
+    def get_permissions(self):
+        # Si el método es LIST o RETRIEVE
+        if(self.action =='list' or self.action =='retrieve'):
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+    
+    # Override de LIST para obtener todas las ofertas
+    def list(self, request):
+        queryset = Article.article_objects.all()
+        serializer = ArticleSerializer(queryset, many=True)
+        
+        return Response(serializer.data)
