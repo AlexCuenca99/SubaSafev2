07ce6820770310528f57ac5dca5443c52679458a
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-30 17:22:21 -0500

    Add manager to model

--- suba_safe_back/applications/comment/models.py
+++ suba_safe_back/applications/comment/models.py
@@ -5,12 +5,15 @@ from model_utils.models import TimeStampedModel
 
 from applications.article.models import Article
 
+from applications.comment.managers import CommentManager
+
 class Comment(TimeStampedModel):
     title = models.CharField('Título', max_length=100)
     content = models.TextField('Contenido', max_length=300)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="articulo_comentarios")
 
+    comment_objects = CommentManager()
     class Meta:
         verbose_name = 'Comentario'
         verbose_name_plural = 'Comentarios'
