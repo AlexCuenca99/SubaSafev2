46a0c766d4a6ee5fbfe0ba523a227285355cbe1c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-29 19:57:44 -0500

    Add CommentProcessSerializer

--- suba_safe_back/applications/comment/serializers.py
+++ suba_safe_back/applications/comment/serializers.py
@@ -7,7 +7,7 @@ from applications.users.models import User
 
 class CommentSerializer(serializers.ModelSerializer):
 
-    users = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
@@ -16,18 +16,24 @@ class CommentSerializer(serializers.ModelSerializer):
             'title',
             'content',
             'user',
-            'users',
             'article',
             'created',
             'modified',
         )
     
-    def get_users(self, obj):
+    def get_user(self, obj):
         query_set = User.comment_objects.users_per_comment(obj.id)
-        serialized_users = UsersInCommentSerializer(query_set, many=True).data
-        return serialized_users
+        serialized_user = UsersInCommentSerializer(query_set, many=True).data
+        return serialized_user
 
 
+# Serializador para crear un comentario
+class CommentProcessSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    content = serializers.CharField(max_length=300)
+    article = serializers.IntegerField()
+    
+    
 class UsersInCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
