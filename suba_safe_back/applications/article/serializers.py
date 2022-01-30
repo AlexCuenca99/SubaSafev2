7b55be716d159a03da9d825cf66fd4f6b4819c2d
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-01-29 23:37:46 -0500

    Add article creation process

--- suba_safe_back/applications/article/serializers.py
+++ suba_safe_back/applications/article/serializers.py
@@ -1,13 +1,12 @@
 # Imports de DRF
-from dataclasses import fields
 from rest_framework import serializers
 
 # Imports de Modelos
 from .models import Article
 
 # Imports de Serializadores
-from applications.category.serializers import CategorySerializer
 from applications.users.serializers import UserPartialDataSerializer
+from applications.category.serializers import CategorySerializer
 
 # Serializador para los artículos
 class ArticleSerializer(serializers.ModelSerializer):
@@ -43,6 +42,25 @@ class ArticleSerializer(serializers.ModelSerializer):
         return serialized_objects
 
 
+# Serializador para crear un artículo
+class ArticleProcessSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=40)
+    description = serializers.CharField(max_length=400)
+    main_image = serializers.ImageField()
+    main_image_opt_text = serializers.CharField(max_length=50)
+    image_1 = serializers.ImageField(required=False, default = 'no_image.png')
+    image_1_opt_text = serializers.CharField(max_length=50, required=False, default = 'Texto alternativo')
+    image_2 = serializers.ImageField(required=False, default = 'no_image.png')
+    image_2_opt_text = serializers.CharField(max_length=50, required=False, default = 'Texto alternativo')
+    image_3 = serializers.ImageField(required=False, default = 'no_image.png')
+    image_3_opt_text = serializers.CharField(max_length=50, required=False, default = 'Texto alternativo')
+    image_4 = serializers.ImageField(required=False, default = 'no_image.png')
+    image_4_opt_text = serializers.CharField(max_length=50, required=False, default = 'Texto alternativo')
+    starting_bid = serializers.DecimalField(max_digits=7, decimal_places=2)
+    category = serializers.IntegerField()
+    
+    
+# Serializador para mostrar todas la imágenes en un arreglo
 class ImagesInArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
--- suba_safe_back/applications/article/viewsets.py
+++ suba_safe_back/applications/article/viewsets.py
@@ -1,22 +1,34 @@
 # Third-Party Apps Imports
+from unicodedata import category
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.permissions import (
     IsAuthenticated,
     AllowAny
 )
-from rest_framework import viewsets, status
+from rest_framework import (
+    viewsets, 
+    status,
+)
 
-
+# Imports de los modelos
+from applications.category.models import Category
 from .models import Article
 
-from .serializers import ArticleSerializer
+from .serializers import (
+    ArticleSerializer,
+    ArticleProcessSerializer,
+)
              
 
 class ArticleViewSet(viewsets.ModelViewSet):
     # Únicamente los usuarios con un token de acceso podrán 
     # usar a las operaciones CRUD
-    authentication_classes = (TokenAuthentication,)
+    authentication_classes = (
+        TokenAuthentication,
+        JWTAuthentication,
+    )
     queryset = Article.article_objects.all()
     serializer_class = ArticleSerializer
 
@@ -36,3 +48,39 @@ class ArticleViewSet(viewsets.ModelViewSet):
         serializer = ArticleSerializer(queryset, many=True)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # Override de CREATE para crear un artículo
+    def create(self, request):
+        serializer = ArticleProcessSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        category_id = serializer.validated_data['category']
+        
+        try:
+            category = Category.objects.get(id=category_id)
+            
+            article = Article.article_objects.create(
+                name = serializer.validated_data['name'],
+                description = serializer.validated_data['description'],
+                main_image = serializer.validated_data['main_image'],
+                main_image_opt_text = serializer.validated_data ['main_image_opt_text'],
+                image_1 = serializer.validated_data['image_1'],
+                image_1_opt_text = serializer.validated_data['image_1_opt_text'],
+                image_2 = serializer.validated_data['image_2'],
+                image_2_opt_text = serializer.validated_data['image_2_opt_text'],
+                image_3 = serializer.validated_data['image_3'],
+                image_3_opt_text = serializer.validated_data['image_3_opt_text'],
+                image_4 = serializer.validated_data['image_4'],
+                image_4_opt_text = serializer.validated_data['image_4_opt_text'],
+                starting_bid = serializer.validated_data['starting_bid'],
+                category = category,
+                seller = self.request.user,
+            )
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
+        except Category.DoesNotExist:
+            content = {'error': 'La Categoría no existe'}
+            
+            return Response(content, status = status.HTTP_404_NOT_FOUND)
+
+        
