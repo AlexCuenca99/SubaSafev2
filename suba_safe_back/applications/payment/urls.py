0691fb6e3b94637fc7b47b754143ee294a9aafaf
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-02-01 15:05:00 -0500

    Add Stripe urls

--- suba_safe_back/applications/payment/urls.py
+++ suba_safe_back/applications/payment/urls.py
@@ -1,9 +1,12 @@
+# Imports de Django
 from django.urls import path
 
-from . import viewsets
+# Imports de views
+from . import views
 
 app_name = 'payment_app'
 
 urlpatterns = [
-    # path('confirmar/pago/', viewsets.PaymentProcessViewSet, name='confirmar-pago'),
+    path('pagos/create-checkout-session/', views.StripeCheckoutSessionAPIView.as_view(), name='create-checkout-session'),
+    path('pagos/crear-sesion-checkout/<articulo>', views.ArticleLandingPageTemplateView.as_view(), name='crear-sesion-checkout'),
 ]
