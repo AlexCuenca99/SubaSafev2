2c89772e0ca9e09d7ace26e65e9f92f039620cf9
AlexCuenca99 <alex-patricio1999@hotmail.com>
2022-02-07 13:39:14 -0500

    Avoid UPDATE and PARTIAL_UPDATE when article is in auction

--- suba_safe_back/applications/article/viewsets.py
+++ suba_safe_back/applications/article/viewsets.py
@@ -86,50 +86,100 @@ class ArticleViewSet(viewsets.ModelViewSet):
             content = {'error': 'La Categoría no existe'}
             
             return Response(content, status = status.HTTP_404_NOT_FOUND)
-
-
+        
     # Override de UPDATE para actualizar un artículo
     def update(self, request, pk=None):
         article = get_object_or_404(Article.article_objects.all(), pk=pk)
-        
-        serializer = ArticleProcessSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        
-        category_id = serializer.validated_data['category']
-        
-        try:
-            category = Category.objects.get(id=category_id)
+                
+        if not article.is_active and article.current_bid is None:
+            serializer = ArticleProcessSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            category_id = serializer.validated_data['category']
+
+            try:
+                category = Category.objects.get(id=category_id)
+                
+                article_data = Article.article_objects.filter(id=pk).update(
+                    name = serializer.validated_data['name'],
+                    description = serializer.validated_data['description'],
+                    main_image = serializer.validated_data['main_image'],
+                    main_image_opt_text = serializer.validated_data ['main_image_opt_text'],
+                    image_1 = serializer.validated_data['image_1'],
+                    image_1_opt_text = serializer.validated_data['image_1_opt_text'],
+                    image_2 = serializer.validated_data['image_2'],
+                    image_2_opt_text = serializer.validated_data['image_2_opt_text'],
+                    image_3 = serializer.validated_data['image_3'],
+                    image_3_opt_text = serializer.validated_data['image_3_opt_text'],
+                    image_4 = serializer.validated_data['image_4'],
+                    image_4_opt_text = serializer.validated_data['image_4_opt_text'],
+                    starting_bid = serializer.validated_data['starting_bid'],
+                    category = category,
+                    seller = self.request.user,
+                )
+                return Response(serializer.data, status = status.HTTP_200_OK)
             
-            article = Article.article_objects.update(
-                name = serializer.validated_data['name'],
-                description = serializer.validated_data['description'],
-                main_image = serializer.validated_data['main_image'],
-                main_image_opt_text = serializer.validated_data ['main_image_opt_text'],
-                image_1 = serializer.validated_data['image_1'],
-                image_1_opt_text = serializer.validated_data['image_1_opt_text'],
-                image_2 = serializer.validated_data['image_2'],
-                image_2_opt_text = serializer.validated_data['image_2_opt_text'],
-                image_3 = serializer.validated_data['image_3'],
-                image_3_opt_text = serializer.validated_data['image_3_opt_text'],
-                image_4 = serializer.validated_data['image_4'],
-                image_4_opt_text = serializer.validated_data['image_4_opt_text'],
-                starting_bid = serializer.validated_data['starting_bid'],
-                category = category,
-                seller = self.request.user,
-            )
-            return Response(serializer.data, status = status.HTTP_200_OK)
-        
-        except Category.DoesNotExist:
-            content = {'error': 'La Categoría no existe'}
-            
-            return Response(content, status = status.HTTP_404_NOT_FOUND)
+            except Category.DoesNotExist:
+                content = {'error': 'La Categoría no existe'}
+                return Response(content, status = status.HTTP_404_NOT_FOUND)
+
+        elif not article.is_active and article.buyer is not None:
+            content = {'errors': 'El artículo no se puede actualizar. Ya existe un pago sobre él.'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            content = {'errors': 'El artículo no se puede actualizar. Existe una subasta en proceso sobre él.'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
     
+    # Override de PARTIAL_UPDATE para actualizar un artículo
+    def partial_update(self, request, pk= None):
+        article = get_object_or_404(Article.article_objects.all(), pk=pk)
+                
+        if not article.is_active and article.current_bid is None:
+            serializer = ArticleProcessSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            category_id = serializer.validated_data['category']
+            
+            try:
+                category = Category.objects.get(id=category_id)
+                
+                article_data = Article.article_objects.filter(id=pk).update(
+                    name = serializer.validated_data['name'],
+                    description = serializer.validated_data['description'],
+                    main_image = serializer.validated_data['main_image'],
+                    main_image_opt_text = serializer.validated_data ['main_image_opt_text'],
+                    image_1 = serializer.validated_data['image_1'],
+                    image_1_opt_text = serializer.validated_data['image_1_opt_text'],
+                    image_2 = serializer.validated_data['image_2'],
+                    image_2_opt_text = serializer.validated_data['image_2_opt_text'],
+                    image_3 = serializer.validated_data['image_3'],
+                    image_3_opt_text = serializer.validated_data['image_3_opt_text'],
+                    image_4 = serializer.validated_data['image_4'],
+                    image_4_opt_text = serializer.validated_data['image_4_opt_text'],
+                    starting_bid = serializer.validated_data['starting_bid'],
+                    category = category,
+                    seller = self.request.user,
+                )
+                return Response(serializer.data, status = status.HTTP_200_OK)
+            
+            except Category.DoesNotExist:
+                content = {'error': 'La Categoría no existe'}
+                return Response(content, status = status.HTTP_404_NOT_FOUND)
+
+        elif not article.is_active and article.buyer is not None:
+            content = {'errors': 'El artículo no se puede actualizar. Ya existe un pago sobre él.'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            content = {'errors': 'El artículo no se puede actualizar. Existe una subasta en proceso sobre él.'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    
+    # Override de DESTROY para eliminar un artículo
     def destroy(self, request, pk=None):
         article = get_object_or_404(Article.article_objects.all(), pk=pk)
         
         if not article.is_active and article.current_bid is None:
             article.delete()
-            content = {'messages': 'El artículo ha sido borrado correcmtamente'}
+            content = {'messages': 'El artículo ha sido borrado correctamente'}
             return Response(content, status=status.HTTP_204_NO_CONTENT)
         elif not article.is_active and article.buyer is not None:
             content = {'errors': 'El artículo no se puede borrar. Existe un pago sobre él.'}
